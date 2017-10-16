98b850dd2831fdead0db29e1dd4b94dfb74d3726
Holger Heißmeyer <holgheiss@gmail.com>
2017-10-16 14:08:41 +0200

    improve seeding and conditions for empty database

--- imports/helper/room.js
+++ imports/helper/room.js
@@ -16,12 +16,14 @@ export function findGlossar(room) {
   const g = findGlossarComponents(mainText, false);
   result = { ...result, ...g };
 
-  for (var i = 0; i < room.subsections.length; i++) {
-    const tab = room.subsections[i];
-    const tabText = localeStr(tab.text);
-    const g = findGlossarComponents(tabText, false);
-    const g2 = findGlossarComponents(tabText, true);
-    result = { ...result, ...g, ...g2 };
+  if (room.subsections) {
+    for (var i = 0; i < room.subsections.length; i++) {
+      const tab = room.subsections[i];
+      const tabText = localeStr(tab.text);
+      const g = findGlossarComponents(tabText, false);
+      const g2 = findGlossarComponents(tabText, true);
+      result = { ...result, ...g, ...g2 };
+    }
   }
 
   return result;
