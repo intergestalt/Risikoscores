bc6b3630b179636b29473de8efcc1cd6e600298e
Holger H <holgheiss@gmail.com>
2018-02-05 15:18:12 +0100

    autosize all textareas (simple solution bypassing react)

--- client/main.js
+++ client/main.js
@@ -2,9 +2,14 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import '../imports/startup/client';
+import autosize from 'autosize';
 
 import App from '../imports/ui/containers/App';
 
 Meteor.startup(() => {
   render(<App />, document.getElementById('render-target'));
 });
+
+setTimeout(function () {
+  autosize(document.querySelectorAll('textarea'));
+}, 1000)
