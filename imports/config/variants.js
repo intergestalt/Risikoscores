9cd11461a0b3fcc98b03752a8f194c566f6d10ff
Holger H <holgheiss@gmail.com>
2019-09-29 20:23:02 +0200

    remove current variants, add "test" variant

--- imports/config/variants.js
+++ imports/config/variants.js
@@ -1,11 +1,12 @@
 const variants = [
   { _id: 'live', name: 'Normal', dir: 'live' },
-  { _id: 'TH', name: 'TH', dir: "TH" },  
+  { _id: 'test', name: 'Test', dir: "test" },  
+  /*{ _id: 'TH', name: 'TH', dir: "TH" },  
   { _id: 'PV', name: 'PV', dir: "PV" },
   { _id: 'HH', name: 'HH', dir: "HH" },
   { _id: 'CH', name: 'CH', dir: "CH" },
   { _id: 'SB', name: 'SB', dir: "SB" },
-  { _id: 'Ariana', name: 'Ariana', dir: "Ariana" },
+  { _id: 'Ariana', name: 'Ariana', dir: "Ariana" },*/
 ]
 
 export { variants }
